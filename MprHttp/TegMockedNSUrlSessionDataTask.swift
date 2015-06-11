5163f18a7040eb0e36b6d272ad3e3a02b086705a
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2015-06-12 09:06:50 +1000

    Add source code

---
+++ MprHttp/TegMockedNSUrlSessionDataTask.swift
@@ -0,0 +1,9 @@
+//
+// Mocked HTTP data task created for a mocked HTTP request.
+//
+
+import UIKit
+
+public class TegMockedNSUrlSessionDataTask: NSURLSessionDataTask {
+  override public func cancel() { }
+}
