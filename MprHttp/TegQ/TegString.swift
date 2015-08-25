3069084f8782044e6ede055c4a72abd91fd61c84
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2015-08-25 10:34:51 +1000

    Support Xcode 7 beta 6

--- MprHttp/TegQ/TegString.swift
+++ MprHttp/TegQ/TegString.swift
@@ -6,12 +6,12 @@ import Foundation
 
 public struct TegString {
   public static func blank(text: String) -> Bool {
-    let trimmed = text.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceCharacterSet())
+    let trimmed = text.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet())
     return trimmed.isEmpty
   }
 
   public static func trim(text: String) -> String {
-    return text.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceCharacterSet())
+    return text.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet())
   }
 
   public static func contains(text: String, substring: String,
@@ -25,17 +25,7 @@ public struct TegString {
 
     return text.rangeOfString(substring, options: NSStringCompareOptions(rawValue: options)) != nil
   }
-  
-  //
-  // Joins elements of the array into a string with separator.
-  // Blank elements are skipped.
-  //
-  public static func joinNonBlank(array: [String], separator: String ) -> String {
-    let nonBlankStrings = array.filter { !self.blank($0) }
-    return separator.join(nonBlankStrings)
-  }
-  
-  
+
   //
   // Returns a single space if string is empty.
   // It is used to set UITableView cells labels as a workaround.
